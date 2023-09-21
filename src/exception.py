ad526c3666af830be2ecb3ed0e9717de9ad34350
Gautam-10137 <pahwagautam786@gmail.com>
2023-09-21 20:49:33 +0530

    Exception & Logging Testing done

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
 # error detail is tracked by sys
+import logger
+import logging  # to log exceptions
 def error_message_detail(error,error_detail:sys):
     # exception info() returns 3 things
     _,_,exc_tb=error_detail.exc_info()
@@ -14,7 +16,16 @@ class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         # Initializing a parent class because the parent class might perform essential initialization tasks
         #  that are required for the child class to function correctly.
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
         return self.error_message
+    
+
+if __name__=="__main__":
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomException(e,sys)
