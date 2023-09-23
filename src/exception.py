b142f666f75912df4c75d6cc0295f24c3d13b55c
Gautam-10137 <pahwagautam786@gmail.com>
2023-09-23 10:59:19 +0530

    Data Ingestion Done

--- src/exception.py
+++ src/exception.py
@@ -24,7 +24,6 @@ class CustomException(Exception):
 if __name__=="__main__":
 
     try:
-        a=1/0
+        pass
     except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys)
+        pass
